455e2fc6add5001e6c4cd8e769c78c907de55ccc
Александр <demkov.sasha42@gmail.com>
2021-04-14 17:34:55 +0300

    It is my second commit. Lecture 1 task 2 done!

---
+++ homework/java/hwl1t2/Algorithms.java
@@ -0,0 +1,44 @@
+package hwl1t2;
+
+public class Algorithms {
+    public static void main(String[] args) {
+        System.out.println("Приготовление чая:");
+        System.out.println("");
+        System.out.println("1) Берём чашку и насыпаем ложечу заварки.");
+        System.out.println("2) Зажигаем газ на плите.");
+        System.out.println("3) Наливаем в чайник воды и ставим на газ.");
+        System.out.println("4) Ждём пока закипит и наливаем в чашку.");
+        System.out.println("5) когда чай запарился, добавляем две ложечки сахара.");
+        System.out.println("6) Размешиваем.");
+        System.out.println("Чай готов.");
+        System.out.println("");
+        System.out.println("Поездка в общественном транспорте:");
+        System.out.println("1) Ждём на остановке нужного нам транспорта.");
+        System.out.println("2) Когда дождались, заходим внутрь.");
+        System.out.println("3) Достаём деньги и оплачиваем за проезд.");
+        System.out.println("4) Если есть свободное место - садимся, если нет, тогда стоим:)");
+        System.out.println("5) Когда приехали на место назначение просим остановиться на остановке.");
+        System.out.println("6) Выходим.");
+        System.out.println("");
+        System.out.println("Игра на компьютере:");
+        System.out.println("1) Включаем комьютер.");
+        System.out.println("2) Ждём пока загрузиться всё.");
+        System.out.println("3) Заходим в игру.");
+        System.out.println("4) Выбираем нужный нам режим.");
+        System.out.println("5) Играем.");
+        System.out.println("");
+        System.out.println("Подъём с утра по-раньше:");
+        System.out.println("1) Проснулись.");
+        System.out.println("2) Сделали зарядку.");
+        System.out.println("3) Застелили кровать.");
+        System.out.println("4) Умылись.");
+        System.out.println("5) Покушали.");
+        System.out.println("");
+        System.out.println("Розыграшь в волейболе:");
+        System.out.println("1) Принятие подачи.");
+        System.out.println("2) Довидение мячя к пасующему.");
+        System.out.println("3) Пас нападающему.");
+        System.out.println("4) Атака.");
+
+    }
+}
