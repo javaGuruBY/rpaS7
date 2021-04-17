b1db430148eb35c065bb990f0d2e62bfeed451a4
danikkrapivnitskiy <danikkrapivnitskiy02007@gmail.com>
2021-04-17 19:13:19 +0200

    Home work has been done

--- homework/java/daniil_krapiunitski/HW2/src/main/java/by/jrr/start/Application.java
+++ homework/java/daniil_krapiunitski/HW2/src/main/java/by/jrr/start/Application.java
@@ -3,6 +3,10 @@ package by.jrr.start;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("lskdjfkjbgk");
+        System.out.println("lskdjfkjbgk");
+        System.out.println("lskdjfkjbgk");
+        System.out.println("lskdjfkjbgk");
+
     }
 }
