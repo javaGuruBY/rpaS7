a12ba124c230e92114deac8e24991564f5786b0c
danikkrapivnitskiy <danikkrapivnitskiy02007@gmail.com>
2021-04-23 18:08:57 +0200

    Home work has been done

---
+++ homework/java/krapiunitski/HW/src/main/java/by/jrr/start/Application.java
@@ -0,0 +1,8 @@
+package krapiunitski.HW.src.main.java.by.jrr.start;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
