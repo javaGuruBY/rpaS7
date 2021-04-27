9824189063dba3b25bfc72bd607e748ecf9be5cc
rezkiy21 <rezkiy21demon>
2021-04-27 23:33:09 +0300

    Menu

--- homework/java/saprykin_vlad/SpringBoot/VladtgBot/src/main/java/by/jrr/VladtgBot/service/TelegramBot.java
+++ homework/java/saprykin_vlad/SpringBoot/VladtgBot/src/main/java/by/jrr/VladtgBot/service/TelegramBot.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 @PropertySource("application.properties")
@@ -24,6 +27,19 @@ public class TelegramBot extends TelegramLongPollingBot {
     private String botToken;
 
     @Autowired
+    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    private long chatId;
+
+    @Autowired
     RequestDispatcher requestDispatcher;
 
     @Autowired
@@ -33,6 +49,52 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         requestDispatcher.dispatch(update);
         saveJson(update);
+
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        chatId = update.getMessage().getChatId();
+
+        String text = update.getMessage().getText();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        try {
+            sendMessage.setText(getMessage(text));
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String getMessage(String msg) {
+        ArrayList<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        if (msg.equals("/start")){
+            keyboard.clear();
+            keyboardFirstRow.add("Анкета");
+            keyboard.add(keyboardFirstRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Выбрать...";
+        }
+        if (msg.equals("Анкета")){
+            keyboard.clear();
+            keyboardFirstRow.add("Да");
+            keyboardFirstRow.add("Нет");
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return "Выбрать...";
+            // переход ко 2 вопросу
+        }
+        if (msg.equals("Да") || msg.equals("Нет")){
+            keyboard.clear();
+            keyboardFirstRow.add("В голове");
+            keyboardFirstRow.add("В компании");
+            return "Выбрать...";
+        }
+        return null;
     }
 
     private void saveJson(Update update) {
@@ -52,4 +114,6 @@ public class TelegramBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return botToken;
     }
+
+
 }
