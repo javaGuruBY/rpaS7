ce4dede4ee4aed697076ed408914dfbb050dbfad
PrinceVIS <saprvlad3007@gmail.com>
2021-04-28 19:44:38 +0300

    Questionaire try

--- homework/java/saprykin_vlad/SpringBoot/VladtgBot/src/main/java/by/jrr/VladtgBot/processors/QuestionnaireProcessor.java
+++ homework/java/saprykin_vlad/SpringBoot/VladtgBot/src/main/java/by/jrr/VladtgBot/processors/QuestionnaireProcessor.java
@@ -1,13 +1,33 @@
 package by.jrr.VladtgBot.processors;
 
+import by.jrr.VladtgBot.service.KeyboardService;
+import by.jrr.VladtgBot.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+
+import static by.jrr.VladtgBot.service.KeyboardService.replyKeyboardMarkup;
 
 @Service
-public class QuestionnaireProcessor implements Processor{
+public class QuestionnaireProcessor implements Processor {
+
+    @Autowired
+    KeyboardService keyboardService;
+
+    @Autowired
+    MessageService messageService;
+
     @Override
     public String run() {
         return "Мы подберём тебе настольную игру :)\n" +
                 "Бот будет присылать тебе вопросы, на которые можно отвечать командами /answerN (N - цифра ответа)\n" +
                 "Ты всегда можешь остановить опрос командой /exitq";
     }
+
+
+
 }
--- homework/java/saprykin_vlad/SpringBoot/VladtgBot/src/main/java/by/jrr/VladtgBot/service/KeyboardService.java
+++ homework/java/saprykin_vlad/SpringBoot/VladtgBot/src/main/java/by/jrr/VladtgBot/service/KeyboardService.java
@@ -1,5 +1,7 @@
 package by.jrr.VladtgBot.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
@@ -8,9 +10,15 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 
+@Service
 public class KeyboardService {
 
-    static final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+    @Autowired
+    TelegramBot telegramBot;
+
+    private long chatId;
+
+    public static final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
     public long getChatId() {
         return chatId;
@@ -19,94 +27,4 @@ public class KeyboardService {
     public void setChatId(long chatId) {
         this.chatId = chatId;
     }
-
-    private long chatId;
-
-    private void sendMsg(Update update) {
-        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
-        Long chatId = update.getMessage().getChatId();
-
-        String text = update.getMessage().getText();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
-
-        try {
-            sendMessage.setText(getMessage(text));
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    String getMessage(String msg) {
-        ArrayList<KeyboardRow> keyboard = new ArrayList<>();
-        KeyboardRow keyboardFirstRow = new KeyboardRow();
-        KeyboardRow keyboardSecondRow = new KeyboardRow();
-
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-        if (msg.equals("/start")){
-            keyboard.clear();
-            keyboardFirstRow.clear();
-            keyboardFirstRow.add("Анкета");
-            keyboard.add(keyboardFirstRow);
-            replyKeyboardMarkup.setKeyboard(keyboard);
-            return "Нажми 'Анкета'";
-        }
-        if (msg.equals("Анкета")) {
-            //requesttdispatcher
-            keyboard.clear();
-            keyboardFirstRow.clear();
-            keyboardFirstRow.add("Начать");
-            replyKeyboardMarkup.setKeyboard(keyboard);
-
-            return "Начнем?";
-        }
-        if (msg.equals("Начать")){
-            // Первый вопрос
-            keyboard.clear();
-            keyboardFirstRow.clear();
-            keyboardFirstRow.add("Да");
-            keyboardFirstRow.add("Нет");
-            replyKeyboardMarkup.setKeyboard(keyboard);
-            return "Ваш выбор...";
-            // переход ко 2 вопросу
-        }
-        if (msg.equals("Да") || msg.equals("Нет")){
-            // Второй вопрос
-            keyboard.clear();
-            keyboardFirstRow.clear();
-            keyboardFirstRow.add("В голове");
-            keyboardFirstRow.add("В компании");
-            return "Ваш выбор...";
-            // переход к 3 вопросу
-        }
-        if (msg.equals("В голове") || msg.equals("В компании")){
-            // Третий вопрос
-            keyboard.clear();
-            keyboardFirstRow.clear();
-            keyboardFirstRow.add("Да");
-            keyboardFirstRow.add("Нет");
-            return "Ваш выбор...";
-            // переход к 4 вопросу
-        }
-        if (msg.equals("Да") || msg.equals("Нет")){
-            // Четвертый вопрос
-            keyboard.clear();
-            keyboardFirstRow.clear();
-            keyboardSecondRow.clear();
-            keyboardFirstRow.add("Уступчивый");
-            keyboardFirstRow.add("Агрессивный");
-            keyboardSecondRow.add("Общительный");
-            keyboardSecondRow.add("Расчетливый");
-            return "Ваш выбор...";
-
-            // Результат
-        }
-
-        return msg;
-    }
-
-
 }
